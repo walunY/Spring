5bf779d4c5e165efe9ae80fa79d06aa0ab06d02a
walun <826223406@qq.com>
2019-04-19 17:38:08 +0800

    bean对象的三大细节：三种创建方式、作用范围、生命周期

---
+++ spring_bean/src/main/java/xyz/walun/factory/InstanceFactory.java
@@ -0,0 +1,15 @@
+package xyz.walun.factory;
+
+import xyz.walun.service.AccountService;
+import xyz.walun.service.impl.AccountServiceImpl;
+
+/**
+ * 模拟一个工厂类(该类可能存在于jar包中，我们无法通过修改源码来提供默认的构造函数)
+ */
+public class InstanceFactory {
+	public InstanceFactory() {
+	}
+	public AccountService getAccountService(){
+		return new AccountServiceImpl();
+	}
+}
---
+++ spring_bean/src/main/java/xyz/walun/service/impl/AccountServiceImpl.java
@@ -0,0 +1,9 @@
+package xyz.walun.service.impl;
+
+import xyz.walun.service.AccountService;
+
+public class AccountServiceImpl implements AccountService {
+	public void saveAccount() {
+		System.out.println("service 中的saveAccount执行了");
+	}
+}
