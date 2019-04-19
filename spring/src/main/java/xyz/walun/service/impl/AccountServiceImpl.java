bbb4e1301b5852e38b881ab96b164044db332d71
walun <826223406@qq.com>
2019-04-19 16:51:46 +0800

    ApplicationContext常用的三个实现类 以及创建对象的核心容器的两个接口比较

---
+++ spring/src/main/java/xyz/walun/service/impl/AccountServiceImpl.java
@@ -0,0 +1,15 @@
+package xyz.walun.service.impl;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import xyz.walun.dao.AccountDao;
+import xyz.walun.service.AccountService;
+
+public class AccountServiceImpl implements AccountService {
+	public void saveAccount() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("Bean.xml");
+		AccountDao dao = (AccountDao) ac.getBean("accountDao");
+		System.out.println(dao);
+		dao.saveAccount();
+	}
+}
